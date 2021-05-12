9ba0a27e6fc8cccc522d093b274052f0082a3f86
Aidos <aidos49@yahoo.com>
2021-05-12 13:27:22 +0300

    updated stage 7

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,27 +1,29 @@
 import _ from 'lodash';
 
+const getName = (path) => path.join('.');
+const getFormattedValue = (value) => {
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  if (_.isPlainObject(value)) {
+    return '[complex value]';
+  }
+  return value;
+};
+
 const makePlain = (diff) => {
   const iter = (currentDiff, path) => {
     const line = currentDiff.flatMap((el) => {
-      const value = _.isPlainObject(el.value) ? '[complex value]' : el.value;
-      let valueOld;
-      let valueNew;
-      if (el.status === 'changed') {
-        valueOld = (typeof el.valueOld === 'string') ? `'${el.valueOld}'` : `${el.valueOld}`;
-        valueOld = _.isPlainObject(el.valueOld) ? '[complex value]' : valueOld;
-        valueNew = (typeof el.valueNew === 'string') ? `'${el.valueNew}'` : `${el.valueNew}`;
-        valueNew = _.isPlainObject(el.valueNew) ? '[complex value]' : valueNew;
-      }
-
+      const newPath = [...path, el.key];
       switch (el.status) {
         case 'nested':
-          return iter(el.children, [...path, el.key]);
+          return iter(el.children, newPath);
         case 'added':
-          return `Property '${[...path, el.key].join('.')}' was added with value: ${(typeof el.value === 'string') ? `'${value}'` : `${value}`}`;
+          return `Property '${getName(newPath)}' was added with value: ${getFormattedValue(el.value)}`;
         case 'deleted':
-          return `Property '${[...path, el.key].join('.')}' was removed`;
+          return `Property '${getName(newPath)}' was removed`;
         case 'changed':
-          return `Property '${[...path, el.key].join('.')}' was updated. From ${valueOld} to ${valueNew}`;
+          return `Property '${getName(newPath)}' was updated. From ${getFormattedValue(el.valueOld)} to ${getFormattedValue(el.valueNew)}`;
         case 'unchanged':
           return [];
         default:
