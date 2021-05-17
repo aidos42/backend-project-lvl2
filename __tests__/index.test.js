9acf42bfdf66e7cec5a58612a4d9c07113c9bea7
Aidos <aidos49@yahoo.com>
2021-05-17 18:23:06 +0300

    minor changes in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import { describe } from 'jest-circus';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import gendiff from '../src/index.js';
@@ -9,19 +10,21 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const inputFormats = ['json', 'yml'];
-const outputFormats = ['stylish', 'plain', 'json'];
-const expectedAnswers = {
-  stylish: readFixture('stylish-result.txt'),
-  plain: readFixture('plain-result.txt'),
-  json: readFixture('json-result.txt'),
-};
+describe('gendiff', () => {
+  const inputFormats = ['json', 'yml'];
+  const outputFormats = ['stylish', 'plain', 'json'];
+  const expectedAnswers = {
+    stylish: readFixture('stylish-result.txt'),
+    plain: readFixture('plain-result.txt'),
+    json: readFixture('json-result.txt'),
+  };
 
-describe.each(inputFormats)('%s data', (inputFormat) => {
-  const path1 = getFixturePath(`file1.${inputFormat}`);
-  const path2 = getFixturePath(`file2.${inputFormat}`);
+  describe.each(inputFormats)('%s data', (inputFormat) => {
+    const path1 = getFixturePath(`file1.${inputFormat}`);
+    const path2 = getFixturePath(`file2.${inputFormat}`);
 
-  test.each(outputFormats)('%s two not empty files', (outputFormat) => {
-    expect(gendiff(path1, path2, outputFormat)).toBe(expectedAnswers[outputFormat]);
+    test.each(outputFormats)('%s two not empty files', (outputFormat) => {
+      expect(gendiff(path1, path2, outputFormat)).toBe(expectedAnswers[outputFormat]);
+    });
   });
 });
