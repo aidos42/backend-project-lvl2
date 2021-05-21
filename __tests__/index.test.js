9e458e67c356f7f242ede56d303c92376234ab34
Aidos <aidos49@yahoo.com>
2021-05-21 15:41:58 +0300

    fixs in tests after 2 review

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,20 +10,26 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('gendiff', () => {
-  const inputFormats = ['json', 'yml'];
-  const outputFormats = ['stylish', 'plain', 'json'];
+  const extensions = ['json', 'yml'];
+  const formats = ['stylish', 'plain', 'json'];
   const expectedAnswers = {
     stylish: readFixture('stylish-result.txt'),
     plain: readFixture('plain-result.txt'),
     json: readFixture('json-result.txt'),
   };
 
-  describe.each(inputFormats)('%s data', (inputFormat) => {
-    const path1 = getFixturePath(`file1.${inputFormat}`);
-    const path2 = getFixturePath(`file2.${inputFormat}`);
+  test('default option', () => {
+    const path1 = getFixturePath('file1.json');
+    const path2 = getFixturePath('file2.json');
+    expect(gendiff(path1, path2)).toBe(expectedAnswers.stylish);
+  });
 
-    test.each(outputFormats)('%s two not empty files', (outputFormat) => {
-      expect(gendiff(path1, path2, outputFormat)).toBe(expectedAnswers[outputFormat]);
+  describe.each(extensions)('%s data', (extension) => {
+    const path1 = getFixturePath(`file1.${extension}`);
+    const path2 = getFixturePath(`file2.${extension}`);
+
+    test.each(formats)('%s two not empty files', (format) => {
+      expect(gendiff(path1, path2, format)).toBe(expectedAnswers[format]);
     });
   });
 });
