a218b0a6d4b99849b2ae3222a658996ed1f32007
Aidos <aidos49@yahoo.com>
2021-05-26 18:20:11 +0300

    & another fixes after 3 review

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { types } from '../buildDiff.js';
 
 const getName = (path) => path.join('.');
-const getFormattedValue = (value) => {
+const stringify = (value) => {
   if (typeof value === 'string') {
     return `'${value}'`;
   }
@@ -21,11 +21,11 @@ export default (diff) => {
         case types.NESTED:
           return iter(el.children, newPath);
         case types.ADDED:
-          return `Property '${getName(newPath)}' was added with value: ${getFormattedValue(el.value)}`;
+          return `Property '${getName(newPath)}' was added with value: ${stringify(el.value)}`;
         case types.REMOVED:
           return `Property '${getName(newPath)}' was removed`;
         case types.UPDATED:
-          return `Property '${getName(newPath)}' was updated. From ${getFormattedValue(el.value.previous)} to ${getFormattedValue(el.value.current)}`;
+          return `Property '${getName(newPath)}' was updated. From ${stringify(el.value.previous)} to ${stringify(el.value.current)}`;
         case types.UNCHANGED:
           return [];
         default:
